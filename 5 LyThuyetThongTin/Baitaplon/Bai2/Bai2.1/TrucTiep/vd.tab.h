44e5707c2ac163f9d531442944d0b39c20115895
ndquang1893 <ndquang1893@gmail.com>
2023-07-09 15:58:35 +0700

    update bai tap chay truc tiep

--- 5 LyThuyetThongTin/Baitaplon/Bai2/Bai2.1/TrucTiep/vd.tab.h
+++ 5 LyThuyetThongTin/Baitaplon/Bai2/Bai2.1/TrucTiep/vd.tab.h
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.5.1.  */
+/* A Bison parser, made by GNU Bison 3.8.2.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2020 Free Software Foundation,
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2021 Free Software Foundation,
    Inc.
 
    This program is free software: you can redistribute it and/or modify
@@ -16,7 +16,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -31,8 +31,9 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Undocumented macros, especially those whose name start with YY_,
-   are private implementation details.  Do not rely on them.  */
+/* DO NOT RELY ON FEATURES THAT ARE NOT DOCUMENTED in the manual,
+   especially those whose name start with YY_ or yy_.  They are
+   private implementation details that can be changed or removed.  */
 
 #ifndef YY_YY_VD_TAB_H_INCLUDED
 # define YY_YY_VD_TAB_H_INCLUDED
@@ -44,29 +45,34 @@
 extern int yydebug;
 #endif
 
-/* Token type.  */
+/* Token kinds.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
   enum yytokentype
   {
-    T_PRINT = 258,
-    T_CONSTANT = 259,
-    T_INT = 260,
-    T_ID = 261,
-    UMINUS = 262
+    YYEMPTY = -2,
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    T_PRINT = 258,                 /* T_PRINT  */
+    T_CONSTANT = 259,              /* T_CONSTANT  */
+    T_INT = 260,                   /* T_INT  */
+    T_ID = 261,                    /* T_ID  */
+    UMINUS = 262                   /* UMINUS  */
   };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 union YYSTYPE
 {
-#line 60 "vd.y"
+#line 54 "vd.y"
 
-	int intValue;
-	char strValue[32];
+    int intValue;
+    char strValue[32];
 
-#line 70 "vd.tab.h"
+#line 76 "vd.tab.h"
 
 };
 typedef union YYSTYPE YYSTYPE;
@@ -77,6 +83,8 @@ typedef union YYSTYPE YYSTYPE;
 
 extern YYSTYPE yylval;
 
+
 int yyparse (void);
+
 
 #endif /* !YY_YY_VD_TAB_H_INCLUDED  */
